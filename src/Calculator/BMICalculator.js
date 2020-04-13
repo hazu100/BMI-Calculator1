b24dcb3eadc46a773206891bb2875b7c0905dea5
Abhisek <abhisek.hazra.95@gmail.com>
2020-04-13 19:20:32 +0530

    newly added string

--- src/Calculator/BMICalculator.js
+++ src/Calculator/BMICalculator.js
@@ -17,7 +17,8 @@ class BMICalculator extends Component {
     calculateBMI() {
         const inputName = this.nameInput.current.value;
         if (inputName.toLowerCase()) {
-            inputName.toLowerCase() === 'piyali' || inputName.toLowerCase() === 'piyaii'?
+            inputName.toLowerCase() === 'piyali' || 
+            inputName.toLowerCase() === 'piyaii'?
                 this.setState({
                     calculatedBMIValue: 'Hi Piyali, you do not need to check your BMI. Just go though this beautiful lines.......You are the cutest..I like the way you smile...I like your mentality...I love your personality...I like the way you counter me ...ha ha ha...The innocent child inside me wants to swim in your dimples...And I like the way you utter TAARPOR while smiling...Do not think too much...Always remain like this..And pray with me so that this Corona goes away from our beautiful planet and everyone can come back into normla life .... '
                 }) :
