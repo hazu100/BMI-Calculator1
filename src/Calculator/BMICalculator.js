e373ce71ed080911039231e31af70c6395f237c9
Abhisek <abhisek.hazra.95@gmail.com>
2020-04-17 21:11:01 +0530

    added bootstrap

--- src/Calculator/BMICalculator.js
+++ src/Calculator/BMICalculator.js
@@ -16,28 +16,27 @@ class BMICalculator extends Component {
     }
 
     calculateBMI() {
-        
+
         const inputName = this.nameInput.current.value;
         if (inputName.toLowerCase()) {
-           if( inputName.toLowerCase() === 'piyali    ')
-            {
+            if (inputName.toLowerCase() === 'piyali    ') {
                 alert("Piyali, Apni Hindi khub e valo bolen....................................................................... r amake fuchka khawaben na ? khawaben na fuchka amake ?\n\n\n Click on Ok to see your BMI");
                 this.props.getRef().current.style.backgroundImage = `url(${Food})`;
-                
+
             }
-            else{
+            else {
                 alert(`Hi ${inputName.toUpperCase()}, We have a surprise for you. Please click on OK button to proceed !`);
-            } 
-            
+            }
+
             inputName.toLowerCase() === 'piyali    ' ?
                 this.setState({
                     calculatedBMIValue: 'Hi Piyali, you do not need to check your BMI. Just go though these beautiful lines.......You are the cutest..I like the way you smile...I like your mentality...I love your personality...I like the way you counter me ...ha ha ha...The innocent child inside me wants to swim in your dimples...And I like the way you utter TAARPOR while smiling...Do not think too much...Always remain like this..And pray with me so that this Corona goes away from our beautiful planet and everyone can come back into normla life .... '
-                }):
+                }) :
                 this.setState({
                     calculatedBMIValue: this.weightInput.current.value / (this.heightInput.current.value * this.heightInput.current.value),
                 });
-        let audio = new Audio(track1);
-        audio.play();        
+            let audio = new Audio(track1);
+            audio.play();
         }
     }
 
@@ -46,11 +45,17 @@ class BMICalculator extends Component {
             <>
                 <h1>BMI Calculator</h1>
                 <div className='calWrapper'>
-                    <input type='text' placeholder="Enter your First Name" ref={this.nameInput} /><span>First Name</span>
-                    <input type='text' placeholder="Enter your height in Meters" ref={this.heightInput} /><span>Height</span>
-                    <input type='text' placeholder="Enter your weight in KGs" ref={this.weightInput} /><span>Weight</span>
-                    <div className="bmi">BMI</div><textarea rows="6" value={this.state.calculatedBMIValue} />
-                    <div><input type='submit' value='Calculate BMI' onClick={this.calculateBMI} /></div>
+                    <span className='row textBoxWrapper'>
+                        <input className="gate" id="class" ref={this.nameInput} type="text" placeholder="First Name" /><label for="class">Name</label>
+                    </span>
+                    <span className='row textBoxWrapper'>
+                        <input className="gate" id="class" type="text" ref={this.heightInput} placeholder="Height in Meters" /><label for="class">Height</label>
+                    </span>
+                    <span className='row textBoxWrapper'>
+                        <input className="gate" id="class" type="text" ref={this.weightInput} placeholder="Weight in Kgs" /><label for="class">Weight</label>
+                    </span>
+                    <div className="bmi"><b>Your BMI is :</b></div><textarea rows="5" value={this.state.calculatedBMIValue} />
+                    <div><input type='submit' className="btn btn-success" value='Calculate BMI' onClick={this.calculateBMI} /></div>
                 </div>
             </>
         );
